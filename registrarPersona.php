7c169b0f33b29abaab5ebbfdeba382c925ab996a
shirlypesoa <edwarselle@gmail.com>
2017-10-07 19:50:23 -0300

    Add files via upload

---
+++ registrarPersona.php
@@ -0,0 +1,22 @@
+<?php
+	$con = mysqli_init();
+	$db_name = "localdb";
+	$mysql_user = "azure";//PaquinBorowicz
+	$mysql_pass = "6#vWHD_$";//micaIERU
+	$server_name = "127.0.0.1:52110";
+	$con = mysqli_real_connect($server_name, $mysql_user, $mysql_pass, $db_name);
+
+
+	$sql_query = "INSERT INTO personas (nombre, apellido, dni, email, usu, contrasena) VALUES ($nombre, $apellido, $dni, $email, $usu, $contrasena)";
+	mysqli_query($con, $sql_query);
+	$result = mysqli_query($con, $sql_query);
+	if (mysqli_num_rows($result)>0)
+	{
+	  echo "Todo salió bien.";
+	}
+	else
+	{
+	  echo "Algo salió mal.";
+	}
+	mysqli_close($con);
+?>
